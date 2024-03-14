74de75b19842919b58e45f907fb2e7d19caa2bb5
meghnadutta02 <meghnakha18@gmail.com>
2024-03-14 23:06:33 +0530

    doctor client page is almost complete

--- server/controllers/doctorController.js
+++ server/controllers/doctorController.js
@@ -4,7 +4,12 @@ const getDoctors = async (req, res, next) => {
   try {
     const specialty = req.query.specialty;
     const searchQuery = req.query.searchQuery;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 12;
+    let totalPages = 0;
+
     const aggregationPipeline = [];
+
     if (searchQuery) {
       const atlasSearchQuery = {
         $search: {
@@ -14,7 +19,9 @@ const getDoctors = async (req, res, next) => {
             path: {
               wildcard: "*",
             },
-            fuzzy: {},
+            fuzzy: {
+              maxEdits: 2,
+            },
           },
         },
       };
@@ -26,19 +33,33 @@ const getDoctors = async (req, res, next) => {
       aggregationPipeline.push({ $match: { specialty: specialty } });
     }
 
-    // If neither searchQuery nor specialty is provided, return all doctors
     if (!searchQuery && !specialty) {
-      const allDoctors = await Doctor.find({}).select(
-        "name specialty chamberAddress contact"
-      );
-      return res.status(200).json(allDoctors);
+      totalPages = Math.ceil((await Doctor.countDocuments()) / limit);
+      const allDoctors = await Doctor.find({})
+        .select("name specialty chamberAddress contact")
+        .skip((page - 1) * limit)
+        .limit(limit);
+      return res.status(200).json({ doctors: allDoctors, totalPages });
     }
 
     aggregationPipeline.push({
       $project: { name: 1, specialty: 1, chamberAddress: 1, contact: 1 },
     });
-    const Doctors = await Doctor.aggregate(aggregationPipeline);
-    res.status(200).json(Doctors);
+    aggregationPipeline.push({ $skip: (page - 1) * limit });
+    aggregationPipeline.push({ $limit: limit });
+    const doctors = await Doctor.aggregate(aggregationPipeline);
+
+    const totalDoctorsCount = await Doctor.aggregate([
+      ...aggregationPipeline,
+      { $count: "total" },
+    ]);
+
+    if (totalDoctorsCount.length > 0) {
+      totalPages = Math.ceil(totalDoctorsCount[0].total / limit);
+    } else {
+      totalPages = 0;
+    }
+    res.status(200).json({ doctors: doctors, totalPages });
   } catch (error) {
     next(error);
   }
@@ -60,4 +81,3 @@ module.exports = {
   getDoctors,
   getDoctorById,
 };
-Hi;
