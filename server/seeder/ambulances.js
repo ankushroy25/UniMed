93ef5e69aa69ff66e5069897cda5f9f35d376ce1
Ankush Roy <imankushroy@gmail.com>
2024-03-17 13:03:48 +0530

    emergency backend

---
+++ server/seeder/ambulances.js
@@ -0,0 +1,73 @@
+const ambulances = [
+  {
+    name: "Ambulance 667",
+    contact: "+1 5533414595",
+    location: {
+      coordinates: {
+        latitude: "22.580990",
+        longitude: "88.297440",
+      },
+    },
+    company: "AMRI",
+    driverName: "Michael Smith",
+    availabilityStatus: false,
+    bookedByUser: "imankushroy@gmail.com",
+  },
+  {
+    name: "Ambulance 680",
+    contact: "+1 9047850585",
+    location: {
+      coordinates: {
+        latitude: "22.582960",
+        longitude: "88.299812",
+      },
+    },
+    company: "Apollo Gleneagles",
+    driverName: "Jane Doe",
+    availabilityStatus: false,
+    bookedByUser: "imankushroy@gmail.com",
+  },
+  {
+    name: "Ambulance 235",
+    contact: "+1 1009865159",
+    location: {
+      coordinates: {
+        latitude: "22.580227",
+        longitude: "88.305316",
+      },
+    },
+    company: "Runy General hospital",
+    driverName: "Jane Doe",
+    availabilityStatus: false,
+    bookedByUser: "imankushroy@gmail.com",
+  },
+  {
+    name: "Ambulance 20",
+    contact: "+1 7526112528",
+    location: {
+      coordinates: {
+        latitude: "22.580690",
+        longitude: "88.357440",
+      },
+    },
+    company: "Fortis",
+    driverName: "John Doe",
+    availabilityStatus: false,
+    bookedByUser: "imankushroy@gmail.com",
+  },
+  {
+    name: "Ambulance 859",
+    contact: "+1 0500509676",
+    location: {
+      coordinates: {
+        latitude: "22.780990",
+        longitude: "88.891440",
+      },
+    },
+    company: "Woodlands",
+    driverName: "John Doe",
+    availabilityStatus: false,
+    bookedByUser: "imankushroy@gmail.com",
+  },
+];
+module.exports = ambulances;
